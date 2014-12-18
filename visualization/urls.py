a76270cb4cf4c838c70db3ae3c6f1e0d8393e17a
dogancankilment <dogancankilment@gmail.com>
2014-12-18 06:06:25 +0200

    visualization urls file deleted, only one urls_filewill be used.

--- visualization/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-# from django.conf import settings
-
-urlpatterns = patterns('',
-                       url(r'^d3',
-                           'visualization.views.d3',
-                           name='d3'),
-                       )
--- workshop/urls.py
+++ workshop/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-                       url(r'^visualization/',
-                           include('visualization.urls')),
-                       )
+                       url(r'^$',
+                           'visualization.views.index',
+                           name='index'),
+                      )
