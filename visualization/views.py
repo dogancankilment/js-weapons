70bacf46bb94f35b71be54137726eabc1cc60371
dogancankilment <dogancankilment@gmail.com>
2014-12-18 06:07:54 +0200

    index page function created

--- visualization/views.py
+++ visualization/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import redirect, render_to_response,render
+
+
+def index(request):
+    return render(request, "d3/tilford_tree.html")
