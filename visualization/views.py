cb68fba08b2b300c0ca800d745a5c182938c986e
dogancankilment <dogancankilment@gmail.com>
2014-12-21 06:42:41 +0200

    new page added for d3_sunburst

--- visualization/views.py
+++ visualization/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import redirect, render_to_response,render
 
 def index(request):
     return render(request, "d3/tilford_tree.html")
+
+
+def sunburst(request):
+    return render(request, "d3/sunburst.html")
